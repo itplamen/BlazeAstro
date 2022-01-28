7ba6f27a69883d921ebb329d0e6e93e55246ac0a
itplamen <itplamen@gmail.com>
2022-01-28 22:14:26 +0200

    Created NASA.APOD data provider

---
+++ BlazeAstro/Services/BlazeAstro.Services.DataProviders/ApodDataProvider.cs
@@ -0,0 +1,44 @@
+﻿namespace BlazeAstro.Services.DataProviders
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Json;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.WebUtilities;
+
+    using BlazeAstro.Services.DataProviders.Contracts;
+    using BlazeAstro.Services.Models.Apod;
+
+    public class ApodDataProvider : IDataProvider<ApodRequestModel, ApodResponseModel>
+    {
+        private const string BASE_URL = "https://api.nasa.gov/planetary/apod";
+
+        private readonly HttpClient httpClient;
+
+        public ApodDataProvider(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<ApodResponseModel> GetData(ApodRequestModel request)
+        {
+            var queryString = new Dictionary<string, string>();
+            var properties = request.GetType().GetProperties();
+
+            foreach (var prop in properties)
+            {
+                string name = prop.CustomAttributes.First().ConstructorArguments.First().Value.ToString();
+                string value = prop.GetValue(request).ToString();
+
+                queryString.Add(name, value);
+            }
+
+            string url = QueryHelpers.AddQueryString(BASE_URL, queryString);
+            var response = await httpClient.GetFromJsonAsync<ApodResponseModel>(url);
+
+            return response;
+        }
+    }
+}
---
+++ BlazeAstro/Services/BlazeAstro.Services.DataProviders/Contracts/IDataProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazeAstro.Services.DataProviders.Contracts
+{
+    using System.Threading.Tasks;
+
+    public interface IDataProvider<TRequest, TResponse>
+        where TRequest : class
+        where TResponse : class
+    {
+        Task<TResponse> GetData(TRequest request);
+    }
+
+    public interface IDataProvider<TResponse>
+        where TResponse : class
+    {
+        Task<TResponse> GetData();
+    }
+}
