cc86839850219dc546486bead5148e160030aa38
itplamen <itplamen@gmail.com>
2022-01-29 22:57:12 +0200

    Created NASA.APOD web models

---
+++ BlazeAstro/Web/BlazeAstro.Web.Models/Apod/ApodInputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazeAstro.Web.Models.Apod
+{
+    using System;
+
+    public class ApodInputModel
+    {
+        public DateTime Date { get; set; }
+
+        public DateTime StartDate { get; set; }
+        
+        public DateTime EndDate { get; set; }
+        
+        public bool Thumbs { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ BlazeAstro/Web/BlazeAstro.Web.Models/Apod/ApodOutputModel.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazeAstro.Web.Models.Apod
+{
+    using System;
+
+    using BlazeAstro.Infrastructure.Mapping;
+    using BlazeAstro.Services.Models.Apod;
+    
+    public class ApodOutputModel : IMapFrom<ApodResponseModel>
+    {
+        public string Title { get; set; }
+
+        public string Explanation { get; set; }
+
+        public string Url { get; set; }
+
+        public string ThumbnailUrl { get; set; }
+
+        public string Copyright { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string MediaType { get; set; }
+    }
+}
