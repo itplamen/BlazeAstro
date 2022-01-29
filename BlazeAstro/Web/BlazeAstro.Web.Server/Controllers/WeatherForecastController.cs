42e689b316ccfecfcfb406e0923a1ae2c10899b9
itplamen <itplamen@gmail.com>
2022-01-29 17:52:31 +0200

    Updated web projects

--- BlazeAstro/Web/BlazeAstro.Web.Server/Controllers/WeatherForecastController.cs
+++ BlazeAstro/Web/BlazeAstro.Web.Server/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
+using BlazeAstro.Web.Shared;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BlazeAstro.Web.Api.Controllers
+namespace BlazeAstro.Web.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -18,7 +19,7 @@ namespace BlazeAstro.Web.Api.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
