6161192f9f5c35b449b4134f753f56bd0250280c
itplamen <itplamen@gmail.com>
2022-01-29 02:25:18 +0200

    Created data provider for current astronauts in space API

---
+++ BlazeAstro/Services/BlazeAstro.Services.DataProviders/AstrosDataProvider.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazeAstro.Services.DataProviders
+{
+    using System.Net.Http;
+    using System.Net.Http.Json;
+    using System.Threading.Tasks;
+
+    using BlazeAstro.Services.DataProviders.Contracts;
+    using BlazeAstro.Services.Models.Astronauts;
+
+    public class AstrosDataProvider : IDataProvider<AstronautsInSpaceResponseModel>
+    {
+        private const string BASE_URL = "https://www.howmanypeopleareinspacerightnow.com/peopleinspace.json";
+
+        private readonly HttpClient httpClient;
+
+        public AstrosDataProvider(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<AstronautsInSpaceResponseModel> GetData()
+        {
+            var response = await httpClient.GetFromJsonAsync<AstronautsInSpaceResponseModel>(BASE_URL);
+
+            return response;
+        }
+    }
+}
