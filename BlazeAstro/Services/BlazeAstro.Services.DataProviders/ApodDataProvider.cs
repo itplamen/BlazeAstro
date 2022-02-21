a85be4bca22d8b5b1bc017170d99608624ba1f56
itplamen <itplamen@gmail.com>
2022-02-22 01:46:41 +0200

    Extracted common logic into query string builder class

--- BlazeAstro/Services/BlazeAstro.Services.DataProviders/ApodDataProvider.cs
+++ BlazeAstro/Services/BlazeAstro.Services.DataProviders/ApodDataProvider.cs
@@ -1,7 +1,6 @@
 ﻿namespace BlazeAstro.Services.DataProviders
 {
     using System.Collections.Generic;
-    using System.Linq;
     using System.Net.Http;
     using System.Threading.Tasks;
 
@@ -11,6 +10,7 @@
     using Newtonsoft.Json.Linq;
 
     using BlazeAstro.Services.DataProviders.Contracts;
+    using BlazeAstro.Services.DataProviders.Utilities;
     using BlazeAstro.Services.Models.Apod;
 
     public class ApodDataProvider : IDataProvider<ApodRequestModel, IEnumerable<ApodResponseModel>>
@@ -24,30 +24,9 @@
 
         public async Task<IEnumerable<ApodResponseModel>> GetData(ApodRequestModel request)
         {
-            var queryString = new Dictionary<string, string>();
-            var properties = request.GetType().GetProperties();
-
-            foreach (var prop in properties)
-            {
-                if (prop.CustomAttributes.Any())
-                {
-                    string name = prop.CustomAttributes.First().ConstructorArguments.First().Value?.ToString();
-                    string value = prop.GetValue(request)?.ToString();
-
-                    if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(value))
-                    {
-                        if (prop.PropertyType != typeof(int) ||
-                            (int.TryParse(value, out int result) &&
-                            result != 0))
-                        {
-                            queryString.Add(name, value);
-                        }
-                    }
-                }
-            }
-
-
+            var queryString = QueryStringBuilder.Build(request);
             string url = QueryHelpers.AddQueryString(request.Url, queryString);
+
             var response = await httpClient.GetAsync(url);
             var content = await response.Content.ReadAsStringAsync();
 
--- BlazeAstro/Services/BlazeAstro.Services.DataProviders/MarsPhotosDataProvider.cs
+++ BlazeAstro/Services/BlazeAstro.Services.DataProviders/MarsPhotosDataProvider.cs
@@ -1,7 +1,5 @@
 ﻿namespace BlazeAstro.Services.DataProviders
 {
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Net.Http;
     using System.Threading.Tasks;
 
@@ -11,6 +9,7 @@
 
     using BlazeAstro.Services.DataProviders.Contracts;
     using BlazeAstro.Services.Models.MarsPhotos;
+    using BlazeAstro.Services.DataProviders.Utilities;
 
     public class MarsPhotosDataProvider : IDataProvider<MarsPhotosRequestModel, MarsPhotosResponseModel>
     {
@@ -23,29 +22,9 @@
 
         public async Task<MarsPhotosResponseModel> GetData(MarsPhotosRequestModel request)
         {
-            var queryString = new Dictionary<string, string>();
-            var properties = request.GetType().GetProperties();
-
-            foreach (var prop in properties)
-            {
-                if (prop.CustomAttributes.Any())
-                {
-                    string name = prop.CustomAttributes.First().ConstructorArguments.First().Value?.ToString();
-                    string value = prop.GetValue(request)?.ToString();
-
-                    if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(value))
-                    {
-                        if (prop.PropertyType != typeof(int) ||
-                            (int.TryParse(value, out int parsed) &&
-                            parsed != 0))
-                        {
-                            queryString.Add(name, value);
-                        }
-                    }
-                }
-            }
-
+            var queryString = QueryStringBuilder.Build(request);
             string url = QueryHelpers.AddQueryString($"{request.Url}/{request.RoverName}/photos", queryString);
+
             var response = await httpClient.GetAsync(url);
 
             var content = await response.Content.ReadAsStringAsync();
